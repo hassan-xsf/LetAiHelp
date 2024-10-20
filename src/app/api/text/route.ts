ecbcf55e2a4cc8aefe706a15324800900c1e24e9
Hassan <hassanshakil4569@gmail.com>
2024-10-21 00:30:54 +0500

    feat(api): add text route

--- src/app/api/text/route.ts
+++ src/app/api/text/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
-
 export async function POST(request: Request) {
     try {
-        const { text , model , type } = await request.json();
+        const { prompt , model , pastMessages , type } = await request.json();
 
+        
         const response = await axios.post<CaptionResponse>(
-            `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/openai/whisper`,
-            { text: text , model: model || "bart-large-cnn" , type},
+            `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/${model ? model : "@cf/google/gemma-2b-it-lora"}`,
+            { prompt , messages: pastMessages || ""},
             {
                 headers: {
                     'Authorization': `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
