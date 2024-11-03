050360aeb031dcd45487f05dcf48ee99d2a70368
Hassan <hassanshakil4569@gmail.com>
2024-11-04 01:58:59 +0500

    chores: revert back cause it wasn't the issue

--- src/app/api/ai-detection/route.ts
+++ src/app/api/ai-detection/route.ts
@@ -42,10 +42,10 @@ export async function POST(request: Request) {
       {
         headers: {
           "Content-Type": "application/json",
-          // "User-Agent":
-          //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
-          // Origin: "https://zerogpt.com",
-          // Host: "api.zerogpt.com",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+          Origin: "https://zerogpt.com",
+          Host: "api.zerogpt.com",
         },
       },
     );
